a5e95a82037fef350b0178b689ccd7972ee7208e
weru <fromweru@gmail.com>
2020-12-06 11:31:54 -0500

    add a js variables definition file

---
+++ assets/js/variables.js
@@ -0,0 +1,14 @@
+// global variables;
+const doc = document.documentElement;
+const toggleId = 'toggle';
+const showId = 'show';
+const menu = 'menu';
+
+// defined in config.toml
+const parentURL = '{{ absURL "" }}';
+
+// defined in i18n / translation files
+const quickLinks = '{{ T "quick_links" }}';
+const searchResultsLabel = '{{ T "search_results_label" }}';
+const typeToSearch = '{{ T "type_to_search" }}';
+const noMatchesFound = '{{ T "no_matches" }}';
