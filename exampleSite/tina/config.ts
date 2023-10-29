16ed331b92fc2f5887f29ddb8ede9ddc722493c8
weru <fromweru@gmail.com>
2023-10-29 20:33:31 +0300

    update branch

--- exampleSite/tina/config.ts
+++ exampleSite/tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 export default defineConfig({
   branch,
