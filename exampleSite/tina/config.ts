da34cd3f65d300092c0f97fbeaef8dbb7fa10559
weru <fromweru@gmail.com>
2023-10-29 19:48:59 +0300

    update tina cms config

---
+++ exampleSite/tina/config.ts
@@ -0,0 +1,115 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "6ff9830b-b18a-4d21-b38c-cae1679e335f", // Get this from tina.io
+  token: "2954980a0db18868974dc57a66be9ecdfe6f336b", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "exampleSite/static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "exampleSite/static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "docs",
+        label: "Docs",
+        path: "exampleSite/content/docs",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Weight"
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description"
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "post",
+        label: "Posts",
+        path: "exampleSite/content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author",
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "tutorials",
+        label: "Tutorials",
+        path: "exampleSite/content/tutorials",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "number",
+            name: "weight",
+            label: "Weight",
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
